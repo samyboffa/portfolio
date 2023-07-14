1c06e859b3b8cb55115ecb76f0c225ac07afbda8
sami gharsallah <samigharsallah01@gmail.com>
2023-07-14 09:07:41 +0100

    rest

--- Components/Projects/ProjectCard.jsx
+++ Components/Projects/ProjectCard.jsx
@@ -1,27 +1,52 @@
-import React, { useEffect, useRef } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { useState } from "react";
+import styleContext from "../../Context/MyContext";
 
-const ProjectCard = ({ project, index }) => {
+const ProjectCard = ({
+  project,
+  index,
+  visibleItem,
+  setvisibleItem,
+  projects,
+}) => {
   const containerRef = useRef();
-  // const [screenHeight, setScreenHeight] = useState(window?.innerHeight);
-  const [isVisible, setisVisible] = useState(false);
+  const { setbgColor } = useContext(styleContext);
+
   const [selected, setselected] = useState(false);
-  const [buttonHovered, setbuttonHovered] = useState(false);
   const [odd] = useState(index % 2 === 0);
+
+  const scrollHandler = (e) => {
+    const rect = containerRef.current.getBoundingClientRect();
+    // console.log(project.name, rect.top, window.innerHeight, rect.bottom);
+
+    if (rect.top < window.innerHeight / 2 && rect.bottom >= 0) {
+      setvisibleItem(index);
+      setbgColor(project.color);
+    }
+  };
+
+  useEffect(() => {
+    const hello = () => {
+      if (visibleItem === index) {
+        console.log(project.name);
+        console.log(projects[visibleItem].name);
+        // const targetElement = document.getElementById(`project${visibleItem}`);
+        // setTimeout(() => {
+        //   targetElement.scrollIntoView({
+        //     behavior: "smooth",
+        //     block: "center",
+        //     inline: "center",
+        //   });
+        // }, 500);
+      }
+    };
+    return hello();
+  }, [visibleItem]);
 
   useEffect(() => {
     window.addEventListener("scroll", scrollHandler);
     return () => window.removeEventListener("scroll", scrollHandler);
-  }, [isVisible]);
-
-  const scrollHandler = () => {
-    const rect = containerRef.current.getBoundingClientRect();
-    if (rect.top < window.innerHeight && rect.bottom >= 0) {
-      setisVisible(true);
-    } else {
-      setisVisible(false);
-    }
-  };
+  }, [visibleItem]);
 
   const keyframeStyles = `
     @keyframes trans {
@@ -56,22 +81,27 @@ const ProjectCard = ({ project, index }) => {
   //
   return (
     <div
+      id={`project${index}`}
+      key={index}
       ref={containerRef}
-      className={`projectContainer flex-lg-nowrap flex-wrap wm100 alignC gap-4 my-5 py-5
+      className={`projectContainer flex-lg-nowrap w100 flex-wrap wm100 alignC my-5 py-5
       ${odd ? "" : "reversed"}
-      ${isVisible ? "" : "notVisibleProject"}
+      ${visibleItem === index ? "" : "notVisibleProject"}
       `}
-      style={{ height: `${project.height}px` }}
+      style={{ minHeight: `${project.height}px` }}
     >
       <style>{keyframeStyles}</style>
       <div
-        className={`projectMain wm100 flex-wrap alignC ${
+        style={{
+          width: !selected ? "100%" : "40%",
+          minHeight: `${project.height}px`,
+        }}
+        className={`projectMain flex-wrap border-blue gap-5 wm100 flex-lg-nowrap alignC ${
           odd ? "" : "reversed"
         }`}
       >
         <div
-          id={`project-${index}`}
-          className={`wm100  projectImage gap-3 alignC  ${
+          className={`wm100 projectImage gap-3 alignC  ${
             odd ? "" : "reversed"
           }`}
           style={{
@@ -81,29 +111,52 @@ const ProjectCard = ({ project, index }) => {
             animation: `${odd ? "trans" : "transrev"} 20s ease-in-out infinite`,
           }}
         >
-          <a href={project.link} target="_blank">
-            {" "}
-            <button id={``} className="projectButton w200">
-              Visit Site
+          {!selected && project.link && (
+            <a href={project.link} target="_blank">
+              {" "}
+              <button id={``} className="projectButton w200">
+                Visit Site
+              </button>
+            </a>
+          )}
+          {!project.link && (
+            <button
+              id={``}
+              className={`projectButton w200`}
+              onClick={() => {
+                setselected(!selected);
+              }}
+            >
+              {odd
+                ? selected
+                  ? "Screenshots >"
+                  : "< Screenshots"
+                : selected
+                ? "< Screenshots"
+                : "Screenshots >"}
             </button>
-          </a>
-          <button
-            id={``}
-            className="projectButton w50px"
-            onClick={() => {
-              setselected(!selected);
-            }}
-          >
-            {odd ? ">" : "<"}
-          </button>
+          )}
         </div>
+
         <div
-          className={`projectInfo w40 wm100 ${
+          className={`projectInfo d-flex flex-column justify-content-center w40 wm100 ${
             odd ? "margEven" : "text-lg-end margOdd"
-          }`}
+          } ${selected ? (odd ? "op0 trans1000" : "op0 trans-1000") : ""}`}
         >
-          <div className="projectTitle w100">{project.name}</div>
-          <div className="projectdate">{project.date}</div>
+          <div className="projectTitle nobreak w100 wm100">{project.name}</div>
+          <div className="projectdate f26">{project.date}</div>
+          <div className="projectdate">{project.type}</div>
+          <div className=" my-3">
+            {project.platform.map((platform, index) => (
+              <img
+                key={index}
+                className="mx-3"
+                src={`/icons/${platform}.png`}
+                alt=""
+                height={50}
+              />
+            ))}
+          </div>
           {project.societyLink && (
             <div className="projectCredit">
               During employment at : <span> </span>
@@ -114,21 +167,29 @@ const ProjectCard = ({ project, index }) => {
           )}
         </div>
       </div>
-      <div
-        className={`screenshotsSection h100 ${
-          isVisible ? "" : "notVisibleProject"
+      {/* <div
+        className={`screenshotsSection  ${
+          visibleItem ? (odd ? "pl100" : "pr100") : "notVisibleProject "
         }`}
         style={{
-          width: selected ? "80vw" : "0vw",
+          width: selected ? "60%" : "0%",
+          height: `${project.height}px`,
           overflowY: selected ? "scroll" : "hidden",
         }}
       >
-        <img src="/projects/screenshots/agilix1.png" alt="" />
-        <img src="/projects/screenshots/agilix1.png" alt="" />
-        <img src="/projects/screenshots/agilix1.png" alt="" />
-        <img src="/projects/screenshots/agilix1.png" alt="" />
-        <img src="/projects/screenshots/agilix1.png" alt="" />
-      </div>
+        {["", "", "", "", ""].map((screen, index) => (
+          <img
+            key={index}
+            src="/projects/screenshots/agilix1.png"
+            style={{
+              height: `${project.height}px`,
+              width: `auto`,
+            }}
+            className="py-1"
+            alt=""
+          />
+        ))}
+      </div> */}
     </div>
   );
 };
--- Components/Projects/ProjectList.jsx
+++ Components/Projects/ProjectList.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import ProjectCard from "./ProjectCard";
 import { projects } from "../../helpers/skills";
+import styleContext from "../../Context/MyContext";
 
 const ProjectList = () => {
+  const [visibleItem, setvisibleItem] = useState(0);
+  const { bgColor } = useContext(styleContext);
   return (
-    <div className="text-white mt-5 projectList overXHidden py-5">
+    <div
+      className="text-white mt-5 projectList overXHidden py-5"
+      style={{ backgroundColor: bgColor }}
+    >
       <h4 className="aboutTitle wm100 text-center">My Projects</h4>
       <div className="d-flex flex-column">
         {projects.map((project, index) => (
-          <ProjectCard project={project} key={index} index={index} />
+          <ProjectCard
+            projects={projects}
+            project={project}
+            key={index}
+            index={index}
+            setvisibleItem={setvisibleItem}
+            visibleItem={visibleItem}
+          />
         ))}
       </div>
     </div>
